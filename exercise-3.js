1aa2c95ee9bc876f083eb00b9c6d47c9b7e0ea0b
Edward Chia <edwardc.jianken@gmail.com>
2025-05-09 23:11:29 +0800

    added js code for exercise 3

--- exercise-3.js
+++ exercise-3.js
@@ -4,7 +4,7 @@ const usdToExchange = prompt(
   "What is the amount of USD to be exchanged to SGD?"
 );
 
-const sgdAfterExchange = usdToExchange * 1.31;
+const sgdAfterExchange = Math.round(usdToExchange * 1.31 * 100) / 100;
 
 console.log(
   usdToExchange + " USD is equivalent to " + sgdAfterExchange + " SGD."
@@ -14,7 +14,7 @@ const sgdToExchange = prompt(
   "What is the amount of SGD to be exchanged to USD?"
 );
 
-const usdAfterExchange = sgdToExchange / 1.31;
+const usdAfterExchange = Math.round((sgdToExchange / 1.31) * 100) / 100;
 
 console.log(
   sgdToExchange + " SGD is equivalent to " + usdAfterExchange + " USD."
